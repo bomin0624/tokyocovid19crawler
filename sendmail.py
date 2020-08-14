eee5ea63c8170786a766750ef1973b6716248e29
Bomin Chuang <appppm31012@gmail.com>
2020-08-14 23:53:43 +0800

    fix some bugs

--- sendmail.py
+++ sendmail.py
@@ -7,17 +7,18 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 
-COMMASPACE = ', '.join
+COMMASPACE = ', '
 
 def main():
     sender = input("Please enter your email here:")
     gmail_password = getpass.getpass("Please enter your password here:")
     recipients = input("Who would you want to send to ?:")
-    
+    recipients_list = []
+    recipients_list.append(recipients)
     # 建立郵件主題
     outer = MIMEMultipart()
     outer['Subject'] = '日本東京都內最新疫情'
-    outer['To'] = COMMASPACE(recipients)
+    outer['To'] = COMMASPACE.join(recipients_list)
     outer['From'] = sender
     outer.preamble = 'You will not see this in a MIME-aware mail reader.\n'
 
@@ -36,7 +37,6 @@ def main():
             outer.attach(msg)
         except:
             print("Unable to open one of the attachments. Error: ", sys.exc_info()[0])
-            raise
 
     composed = outer.as_string()
 
@@ -47,7 +47,7 @@ def main():
             s.starttls()
             s.ehlo()
             s.login(sender, gmail_password)
-            s.sendmail(sender, recipients, composed)
+            s.sendmail(sender, recipients_list, composed)
             s.close()
         print("Email sent!")
     except:
--- webcrawler.py
+++ webcrawler.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import time
+import csv
 import pandas as pd
 from selenium import webdriver
 from lxml import etree
+
 
 chromedriver = '/usr/local/bin/chromedriver'
 driver = webdriver.Chrome(chromedriver)
@@ -11,19 +13,21 @@ driver.get("https://stopcovid19.metro.tokyo.lg.jp/zh-tw")
 
 # Wait the time to fresh
 time.sleep(10)
-
 selector = etree.HTML(driver.page_source)
 
 total_people = selector.xpath('//*[@id="tab-0"]/div/div/div[1]/div/div/div[4]/ul/li/div/span[2]/strong/text()')
 everyday_number = selector.xpath('//*[@id="tab-0"]/div/div/div[2]/div/div/div[1]/div/div/span/text()')
 date_and_daynumber = selector.xpath('//*[@id="tab-0"]/div/div/div[2]/div/div/div[1]/div/div/small/text()')
 
-coronalist = []
+with open('/Users/bomin/Desktop/covid19.csv', newline='') as f:
+    reader = csv.reader(f)
+    coronalist = list(reader)
 coronalist.append([total_people,everyday_number,date_and_daynumber])
 
 driver.close()
+print ('done!')
 
 co = pd.DataFrame(coronalist)
-co.columns = ['Total number','Daily infection','Date(increase or decrease)']
+#co.columns = ['Total number','Daily infection','Date(increase or decrease)']
 co.to_csv('/Users/bomin/Desktop/covid19.csv',index = False,encoding='utf_8_sig')
 coronalist
